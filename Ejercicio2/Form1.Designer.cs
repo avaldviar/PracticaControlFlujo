72f2a842bd041bd05bf92cb5911d900a8b015bea
Armando <Avaldiviar@uamv.edu.ni>
2024-09-13 01:42:50 -0600

    Completed PracticaControlFlujo exercise

---
+++ Ejercicio2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Evaluacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstStudent = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar nombre:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(758, 49);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(84, 66);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 145);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lista de nombres:";
+            // 
+            // lstStudent
+            // 
+            this.lstStudent.FormattingEnabled = true;
+            this.lstStudent.ItemHeight = 20;
+            this.lstStudent.Location = new System.Drawing.Point(294, 145);
+            this.lstStudent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstStudent.Name = "lstStudent";
+            this.lstStudent.Size = new System.Drawing.Size(382, 484);
+            this.lstStudent.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtName.Location = new System.Drawing.Point(294, 49);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(382, 26);
+            this.txtName.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lstStudent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstStudent;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ Ejercicio2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using Evaluacion.agregar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion
+{
+    public partial class Form1 : Form
+    {
+        //Se almacena los estudiantes agregados
+        private List<Student> estudiantes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Se Inicializa la lista de estudiantes
+            estudiantes = new List<Student>();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void MostrarNombresLargos()
+        {
+            // Limpiamos la lista visual antes de actualizarla.
+            lstStudent.Items.Clear();
+
+            // Recorremos la lista de estudiantes y mostramos solo los nombres largos.
+            for (int i = 0; i < estudiantes.Count; i++)
+            {
+                Student estudiante = estudiantes[i];
+                if (estudiante.TieneNombreLargo())
+                {
+                    lstStudent.Items.Add(estudiante.NombreCompleto); // Agregamos el nombre al ListBox.
+                }
+
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+           
+            string nombreCompleto = txtName.Text.Trim();
+
+            // Se crea un nuevo estudiante.
+            Student estudiante = new Student(nombreCompleto);
+            estudiantes.Add(estudiante);
+
+            txtName.Clear();
+            MostrarNombresLargos();
+        }
+    }
+}
